79e36965809ca2948e67c9a09bb10330ceb203b9
Ryan <ryan3738@gmail.com>
2021-08-16 08:07:32 -0700

    install and setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,17 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
     ],
   },
 };
